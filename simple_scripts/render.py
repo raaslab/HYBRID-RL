b189d180ab6f7fc89d3b2d15c997b699fcaa7928
amisha2019 <amishabhaskar@gmail.com>
2024-08-01 10:55:26 -0400

    render_image

---
+++ simple_scripts/render.py
@@ -0,0 +1,23 @@
+import metaworld
+import numpy as np
+import random
+import time
+from PIL import Image
+
+# Initialize the MT1 benchmark for the 'assembly-v2' task
+mt1 = metaworld.MT1('assembly-v2')
+
+# Create an environment with task `assembly-v2`
+env = mt1.train_classes['assembly-v2']()
+task = random.choice(mt1.train_tasks)
+env.set_task(task)
+
+# High resolution render
+img = env.sim.render(width=1920, height=1080, camera_name='corner2')
+
+# Convert the image to RGB format and save it
+image = Image.fromarray(img)
+image.save('assembly_env_render.pdf')
+
+time.sleep(10)
+env.close()
